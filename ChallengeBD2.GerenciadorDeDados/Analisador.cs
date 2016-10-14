40f01cb2be4a2a8ab4649589db6605c8b4d28ff3
Isaque Felix Seabra <isaque.seabra@hotmail.com>
2016-10-14 19:58:16 -0300

    Ajustes geral

--- ChallengeBD2.GerenciadorDeDados/Analisador.cs
+++ ChallengeBD2.GerenciadorDeDados/Analisador.cs
@@ -13,30 +13,34 @@ namespace ChallengeBD2.GerenciadorDeDados
         {
             using (var context = new Challenge_BDEntities())
             {
-                //Busca postagens não processadas
+                //Busca postagens processadas
                 var TodasPostagens = context.PostagensProcessadas.AsNoTracking().Include("Postagens").AsNoTracking().ToList();
 
-                //Busca as Classificação dos termos
+                //Busca as Termos classificados
                 var Termos = context.ClassificacaoTermo.ToList();
 
-                //Varrer todas as postagens processados para adicionar ou retirar peso
+                //Varrer todas as postagens processadas para adicionar ou retirar peso.
                 foreach (var postagem in TodasPostagens)
                 {
+                    //Procurar em cada postagem os termos a serem analisados
                     foreach (var termo in Termos)
                     {
                         if(postagem.PostProcessado.Contains(" "+ termo.Termo +" "))
                         {
+                            //Cria nova item de vinculo das tabelas
                             var pesoPostagens = new PesoPostagens();
                             pesoPostagens.IdTermo = termo.Id;
                             pesoPostagens.IdPost = postagem.IdPost;
                             pesoPostagens.Peso = termo.Peso;
                             pesoPostagens.Instituicao= postagem.Postagens.Instituicao;
 
+                            //Adiciona Tabela de Peso e Postagem
                             context.PesoPostagens.Add(pesoPostagens);
                         }
                     }
                 }
 
+                //Salva no banco de dados
                 context.SaveChanges();
             }
         }
